bf4b549fee02adb5393c1020072b1ac2e41895a2
frostid <muhabbik2004@gmail.com>
2021-01-22 08:00:27 +0700

    Add files via upload

---
+++ anykernel.sh
@@ -0,0 +1,36 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Moonlight Kernel EAS - by frost_id @ telegram
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=whyred
+device.name2=lavender
+supported.versions=9 - 11
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+# end ramdisk changes
+
+write_boot;
+## end install
+
