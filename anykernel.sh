d61c229d61e795e22016be359b48ce274a7dcf83
Najah Muhabbik <72481862+frostid@users.noreply.github.com>
2021-01-31 01:24:00 +0700

    Asssuuuu

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=xxx Moonlight Kernel EAS for whyred and lavender xxxx
+kernel.string=xxx Moonlight Kernel EAS for sdm660 xxxx
 do.devicecheck=1
 do.modules=0
 do.systemless=1
