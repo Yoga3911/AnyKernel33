47df3877e0f487f80acb04bb43593b2c086daeaa
Yoga <74900723+NekomataID@users.noreply.github.com>
2021-02-08 00:46:04 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=xxx Moonlight Kernel EAS for XiaoMi sdm660 xxx
+kernel.string=Excalibur Kernel EAS Lavender
 do.devicecheck=1
 do.modules=0
 do.systemless=1
