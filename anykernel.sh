24d925f73d473eab7a92d71b5a207075aafc58c9
Devil  Rush™ <69719056+Xenoms@users.noreply.github.com>
2021-04-19 06:28:24 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Excalibur Kernel | Lavender
+kernel.string=RolleStrix Kernel | Lavender
 do.devicecheck=1
 do.modules=0
 do.systemless=1
